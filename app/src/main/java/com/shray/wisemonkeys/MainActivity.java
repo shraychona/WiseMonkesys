1e391713213d23b491b0b872e9b629381ed88bbc
Shray <shraychona@gmail.com>
2017-04-17 23:30:13 +0530

    Initial Commit

--- app/src/main/java/com/shray/wisemonkeys/MainActivity.java
+++ app/src/main/java/com/shray/wisemonkeys/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    EditText email,password;
+    EditText mEditTextEmail,password;
     Button login;
     String mEmail,mPassword;
 
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email=(EditText)findViewById(R.id.email);
+        mEditTextEmail =(EditText)findViewById(R.id.email);
         password=(EditText)findViewById(R.id.password);
         login=(Button)findViewById(R.id.loginBtn);
         login.setOnClickListener(this);
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (id){
             case R.id.loginBtn:
-                mEmail=email.getText().toString().trim();
+                mEmail= mEditTextEmail.getText().toString().trim();
                 mPassword=password.getText().toString().trim();
                 System.out.println(mEmail+" "+mPassword);
                 break;
